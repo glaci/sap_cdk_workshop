28ee3be32129cc46134de913ff05a3022fc19499
Lin <jamsli@a483e7814263.ant.amazon.com>
2020-05-05 14:12:08 +0800

    eat profile variable

--- createKeyPair.py
+++ createKeyPair.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
-import boto3
+import boto3,sys
 
-boto3.setup_default_session(profile_name='security1')
+profile=sys.argv[1]
 
+boto3.setup_default_session(profile_name=profile)
+
+#create key pair
 ec2 = boto3.client('ec2')
-
-
-
 new_keypair = ec2.create_key_pair(KeyName='keyWorkspace')
 
+# save to local
 with open('./key_workspace.pem', 'w') as file:
     file.write(new_keypair.get('KeyMaterial'))
 
-print(new_keypair.get('KeyMaterial'))
+#print(new_keypair.get('KeyMaterial'))
